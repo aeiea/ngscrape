9323fe612a1fbf3e252ef502665b574d6e820568
aeiea <118844761+aeiea@users.noreply.github.com>
2023-05-27 17:01:20 -0700

    it works

--- ngscrape.py
+++ ngscrape.py
@@ -26,8 +26,7 @@ class Scraper:
         if debug:
             self.debug = True
             return
-        self.debug = False
-        print('NGScrape Initalized') 
+        self.debug = False 
         return
     
     def scrape_game_by_url(self, url: str, download: str, filename: str) -> None:
@@ -38,11 +37,19 @@ class Scraper:
             url (str): The URL of the flash game. For example, the URL for Alien Homonid is https://www.newgrounds.com/portal/view/59593.
             download (str): The directory to download the file to.
             filename (str): The name of the downloaded file.
+        Example parameters:
+            url = 'https://www.newgrounds.com/portal/view/59593'
+            download = 'testdir'
+            filename = 'game.swf'
+        Example output with debug mode:
+            NGScrape: Made request to https://www.newgrounds.com/portal/view/59593 and got status code 200
+            NGScrape: Found flash game link https:\/\/uploads.ungrounded.net\/59000\/59593_alien_booya.swf?f1101313499
+            NGScrape: Downloaded swf file to testdir/game.swf
         '''
         _gameHTML = requests.get(url)
         _soup = bs4.BeautifulSoup(_gameHTML.content, 'html.parser')
         if self.debug:
-            print('NGScrape: Made request to ' + url + ' and got ' + _gameHTML.content + ' with status code ' + _gameHTML.status_code)
+            print('NGScrape: Made request to ' + url + ' and got status code ' + str(_gameHTML.status_code))
         '''
         NOTE: Newgrounds site format has the flash game url inside <script> tags, with the varible 'embed_controller'.
         Example:
@@ -51,7 +58,14 @@ class Scraper:
         '''
         _scripts = _soup.find_all('script')
         for i in _scripts:
-            if 'var embed_controller = new embedController([{"url":"' in i.string:
-                _gameLink = i.string.split('"')[3]
+            if 'var embed_controller = new embedController([{"url":"' in i.get_text():
+                _gameLink = i.get_text().split('"')[3]
                 if self.debug:
-                    print('NGScrape: Found flash game link:' + _gameLink)
+                    print('NGScrape: Found flash game link ' + _gameLink)
+        open(download + '/' + filename, 'wb').write(requests.get(_gameLink, allow_redirects = True).content)
+        if self.debug:
+            print('NGScrape: Downloaded swf file to ' + download + '/' + filename)
+                
+if __name__ == '__main__':
+    scraper = Scraper(debug = True)
+    scraper.scrape_game_by_url('https://www.newgrounds.com/portal/view/59593', 'testdir', 'game.swf')
