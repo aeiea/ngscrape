f063f144395fa6befe0835dc077dc6af15bc7e56
aeiea <118844761+aeiea@users.noreply.github.com>
2023-05-30 17:29:39 -0700

    scrape game card+desc

--- src/ngscrape/__init__.py
+++ src/ngscrape/__init__.py
@@ -23,6 +23,7 @@ class Scraper:
         Start a new NGScrape Instance.
         '''
         if debug:
+            print('')
             self.debug = True
             return
         self.debug = False 
@@ -61,6 +62,7 @@ class Scraper:
                 _gameLink: str = i.get_text().split('"')[3]
                 if self.debug:
                     print('NGScrape: Found flash game link ' + _gameLink)
+                break
         try:
             os.mkdir(download)
         except:
@@ -68,8 +70,88 @@ class Scraper:
         open(download + '/' + filename, 'wb').write(requests.get(_gameLink.replace('\\', ''), allow_redirects = True).content)
         if self.debug:
             print('NGScrape: Downloaded swf file to ' + download + '/' + filename)
-                
+            
+    def scrape_card_by_url(self, url: str, download: str, filename: str) -> str:
+        '''
+        Scrape a flash game's card by url. The file extention will be automatically determined.
+        Returns the name of the card file.
+        Parameters:
+            url (str): The URL of the flash game. For example, the URL for Alien Homonid is https://www.newgrounds.com/portal/view/59593.
+            download (str): The directory to download the file to.
+            filename (str): The name of the downloaded file. The file extention will be automatically determined.
+        Example parameters:
+            url = 'https://www.newgrounds.com/portal/view/59593'
+            download = 'testdir'
+            filename = 'card'
+        Example output with debug mode:
+            NGScrape: Made request to https://www.newgrounds.com/portal/view/59593 and got status code 200
+            NGScrape: Found card link https://picon.ngfiles.com/59000/flash_59593_card.png?f1607717241
+            NGScrape: Downloaded swf file to testdir/card.png
+        '''
+        try:
+            os.mkdir(download)
+        except:
+            pass
+        _gameHTML = requests.get(url)
+        if self.debug:
+            print('NGScrape: Made request to ' + url + ' and got status code ' + str(_gameHTML.status_code))
+        _soup = bs4.BeautifulSoup(_gameHTML.content, 'html.parser')
+        
+        _metadata = _soup.find_all('meta')
+        for i in _metadata:
+            try:
+                if i['property'] == 'og:image':
+                    _gameLink = i['content']
+                    if self.debug:
+                        print('NGScrape: Found card link ' + _gameLink)
+                    break
+            except:
+                pass # This is because some tags don't have the property attribute
+        _commonImageFileTypes = ['.png', '.jpg', '.jfif', '.webp', '.jpeg', '.jpe', '.jif', '.jfi', '.bmp']
+        for i in _commonImageFileTypes:
+            if i in _gameLink:
+                _imageFiletype = i
+        open(download + '/' + filename + _imageFiletype, 'wb').write(requests.get(_gameLink, allow_redirects = True).content)
+        if self.debug:
+            print('NGScrape: Downloaded swf file to ' + download + '/' + filename + _imageFiletype)
+        return filename + _imageFiletype
+    def scrape_desc_by_url(self, url: str) -> str:
+        '''
+        Scrape a flash game's description by url. The file extention will be automatically determined.
+        Returns the description of the game.
+        Parameters:
+            url (str): The URL of the flash game. For example, the URL for Alien Homonid is https://www.newgrounds.com/portal/view/59593.
+        Example parameters:
+            url = 'https://www.newgrounds.com/portal/view/59593'
+        Example output with debug mode:
+            NGScrape: Made request to https://www.newgrounds.com/portal/view/59593 and got status code 200
+            NGScrape: Found game description "Blast FBI agents in this Metal Slug style shooter!"
+        '''
+
+        _gameHTML = requests.get(url)
+        if self.debug:
+            print('NGScrape: Made request to ' + url + ' and got status code ' + str(_gameHTML.status_code))
+        _soup = bs4.BeautifulSoup(_gameHTML.content, 'html.parser')
+        
+        _metadata = _soup.find_all('meta')
+        for i in _metadata:
+            try:
+                if i['property'] == 'og:description':
+                    _desc = i['content']
+                    if self.debug:
+                        print('Found game description "' + _desc + '"')
+                    return _desc
+            except:
+                pass # This is because some tags don't have the property attribute
+        
 if __name__ == '__main__':
     import sys
     scraper = Scraper(debug = True)
-    scraper.scrape_game_by_url(sys.argv[1], sys.argv[2], sys.argv[3])
+    try:
+        scraper.scrape_game_by_url(sys.argv[1], sys.argv[2], sys.argv[3])
+        scraper.scrape_card_by_url(sys.argv[1], sys.argv[2], sys.argv[3].replace('.swf', ''))
+        scraper.scrape_desc_by_url(sys.argv[1])
+    except:
+        print('ngscrape [newgrounds url, should be formatted like this: https://www.newgrounds.com/portal/view/xxxxx] [directory to save to] [file to save to]')
+        exit(1)
+    exit(0)
